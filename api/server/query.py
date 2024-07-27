746c3d38c12b1cd3c1b41b40528d3feba0545ee6
YMY arctto <2974466588@qq.com>
2024-07-28 03:09:42 +0800

    fix：更改文档

--- api/server/query.py
+++ api/server/query.py
@@ -2,7 +2,7 @@ from dao import query
 from const import const
 
 Header = query.QueryInit(False)
-Body = query.QueryInit("未识别字段\n发送<帮助>以获取列表")
+Body = query.QueryInit("未识别字段\n发送“帮助”以获取列表")
 
 
 def init():
@@ -22,8 +22,10 @@ def body_init():
         " 关于               Bot信息    \n" + \
         " 酒水   [酒水tag]   查看满足tag的酒水 \n" + \
         "         可多选    查看满足所有tag的酒水 \n" + \
+        " 角色    别名列表     查看所有别名   \n" + \
         " 角色    [角色]      查看[角色]信息   \n" + \
-        " 角色   <食物tag>    推荐上菜食物    \n"
+        " 角色    <别名>      查看[角色]别名    \n" + \
+        " 角色   <{食物tag}>    推荐上菜食物    \n"
 
     _about = "这里是ling_xian_bot，一款东方夜雀食堂信息查询bot，发送[帮助]可查看使用说明\n项目地址：\n" + \
              "https://github.com/YMYarctto/QQBot_test"
@@ -31,6 +33,7 @@ def body_init():
     Body.Set("帮助", query.QueryInit(f'{_help}'))
     Body.Set("关于", query.QueryInit(f'{_about}'))
     Body.Set("酒水", const.ID_DRINK)
+    Body.Set("角色", const.ID_CUSTOMER)
     # drink.Set_All(["无酒精", "低酒精", "中酒精", "高酒精", "可加冰", "可加热", "烧酒", "利口酒", "清酒",
     #                "西洋酒", "啤酒", "鸡尾酒", "直饮", "水果", "甘", "辛", "苦", "气泡", "古典", "提神", "现代"],
     #               const.ID_DRINK)
