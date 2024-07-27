65a807a6128f5e48de0f0b51850d54996ff92faf
YMY arctto <2974466588@qq.com>
2024-07-28 03:10:06 +0800

    feat：新增功能customer

---
+++ api/server/customer.py
@@ -0,0 +1,31 @@
+from dao import customer
+
+Customer = customer.CustomerListInit()
+CustomerQuery = customer.CustomerQueryInit("")
+
+
+def customer_query_init():
+    CustomerQuery.Set("别名列表", True)
+    inner = CustomerQuery.Group("", "发送“角色 {角色名/角色别名}”以查看角色信息")
+    inner.Set("别名", True)
+
+
+def customer_init():
+    Customer.Set_Customer("莉格露", {
+        "name": "莉格露",
+        "other_name": ["莉格露", "lgl", "虫子", "萤火虫"],
+        "like_food_tag": ["肉", "甜", "生", "猎奇"],
+        "unlike_food_tag": ["素", "清淡", "凉爽"],
+        "like_drink_tag": ["低酒精", "可加冰"],
+        "place": "妖兽怪道|魔法森林",
+        "s_card": "灯符『荧光现象』:顾客出现速度+30%，持续30秒",
+        "f_card": "蠢符『夜虫龙卷』:赶走所有客人且不结账",
+        "money": "200~400円",
+        "food": ["露水煮蛋", "香炸蝉蜕", "幻昙华糕"],
+    })
+    Customer.Set_other_name("莉格露")
+
+
+def customer_server_init():
+    customer_query_init()
+    customer_init()
---
+++ dao/customer.py
@@ -0,0 +1,125 @@
+from tools import tools as t
+
+# Customer: {
+#     "__other_name": {
+#         "NAME":"",
+#         "like_food_tag": "",
+#         "unlike_food_tag": "",
+#         "like_drink_tag": "",
+#         "PLACE": "",
+#         "S_CARD": "",
+#         "F_CARD": "",
+#         "MONEY": "",
+#         "food": "",
+#     },
+#     "__customer": {
+#         "a": "a"
+#     }
+# }
+
+
+class CustomerList:
+    class Customer:
+        def __init__(self, v):
+            self.NAME = v["name"]
+            self.other_name = v["other_name"]
+            self.like_food_tag = v["like_food_tag"]
+            self.unlike_food_tag = v["unlike_food_tag"]
+            self.like_drink_tag = v["like_drink_tag"]
+            self.PLACE = v["place"]
+            self.S_CARD = v["s_card"]
+            self.F_CARD = v["f_card"]
+            self.MONEY = v["money"]
+            self.food = v["food"]
+
+        def __str__(self):
+            s_like_food = "|"
+            for v in self.like_food_tag:
+                s_like_food += f'{v}|'
+            s_unlike_food = "|"
+            for v in self.unlike_food_tag:
+                s_unlike_food += f'{v}|'
+            s_like_drink = "|"
+            for v in self.like_drink_tag:
+                s_like_drink += f'{v}|'
+            s_food = "|"
+            for v in self.food:
+                s_food += f'{v}|'
+            return (f'{self.NAME}\n\n'
+                    f'☆偏好：\n'
+                    f' ·菜肴喜好：{s_like_food}\n'
+                    f' ·菜肴厌恶：{s_unlike_food}\n'
+                    f' ·酒水喜好：{s_like_drink}\n'
+                    f'☆持有：{self.MONEY}\n'
+                    f'☆出现地区：{self.PLACE}\n'
+                    f'☆菜谱：{s_food}\n'
+                    f'☆符卡：\n'
+                    f' ·奖励符卡：{self.S_CARD}\n'
+                    f' ·惩罚符卡：{self.F_CARD}')
+
+        def __other_name_str__(self):
+            s = "查询到以下别名：\n| "
+            for name in self.other_name:
+                s += f'{name} | '
+            return s
+
+    def __init__(self):
+        self.__customer = dict()
+        self.__other_name = dict()
+
+    def Set_other_name(self, s):
+        for name in self.__customer[s].other_name:
+            self.o_name(name, s)
+
+    def o_name(self, other_name, s):
+        self.__other_name[other_name] = s
+
+    def Set_Customer(self, name, v):
+        self.__customer[name] = self.Customer(v)
+
+    def Search_Customer(self, name):
+        if name in self.__other_name.keys():
+            return self.__customer[self.__other_name[name]]
+        if name in self.__customer.keys():
+            return self.__customer[name]
+        return Exception("未找到角色，发送“角色 别名列表”以查看所有别名")
+
+    def __other_name_str__(self):
+        s = "别名列表：\n"
+        for name in self.__customer:
+            s += f'{name}：| '
+            for o_name in self.__customer[name].other_name:
+                s += f'{o_name} | '
+            s += "\n"
+        return s
+
+
+class CustomerQuery:
+    def __init__(self, content):
+        self.__dict = dict()
+        self.Set("", content)
+
+    def Set(self, query, content):
+        self.__dict[query] = content
+
+    def Group(self, query, content):
+        self.__dict[query] = CustomerQueryInit(content)
+        return self.__dict[query]
+
+    def Analysis(self, query):
+        if not self.Contain(query):
+            return False
+        elif not t.type_name(self.__dict[query], "str"):
+            return self.__dict[query]
+        return self.__dict[""]
+
+    def Contain(self, query):
+        return query in self.__dict
+
+
+def CustomerQueryInit(content):
+    return CustomerQuery(content=content)
+
+
+def CustomerListInit():
+    return CustomerList()
