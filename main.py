85f64953b4775929bfefc478e6f521811d3eb1fd
YMY arctto <2974466588@qq.com>
2024-07-27 18:45:02 +0800

    fix：修改逻辑

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import pyperclip as lip
 import keyboard
 import time
 
-from tools import content as cont
-from api.server import init as server
+from const import const as cst
+from tools import tools as tools
+from api.server import query as server
+from api.server import drink as server_drink
 
 
 def send_message(content):
@@ -18,24 +20,28 @@ def server_start():
     print("begin")
 
     while True:
-        try:
-            box = tuple[int, int, int, int](gui.locateOnScreen("./image/qqHy_24.png", confidence=0.8))
-            gui.click(gui.center(box))
-            content = try_lingXian()
-            if type(content).__name__ == "str":
-                time.sleep(0.5)
-                gui.click(gui.center(box))
-                send_message("[ling_xian_bot]\n\n"+content)
-            keyboard.press_and_release("esc")
-        except gui.ImageNotFoundException:
-            continue
+        try_image("./image/qqHy_24.png")
+        try_image("./image/qqQ_24.png")
 
+
+def try_image(img_url):
+    try:
+        box = tuple[int, int, int, int](gui.locateOnScreen(img_url, confidence=0.9))
+        gui.click(gui.center(box))
+        content = try_lingXian()
+        if tools.type_name(content, "str"):
+            time.sleep(0.5)
+            gui.click(gui.center(box))
+            send_message("[ling_xian_bot]\n\n" + content)
+        keyboard.press_and_release("esc")
+    except gui.ImageNotFoundException:
+        return
 
 def try_lingXian():
     try:
-        box = tuple[int, int, int, int](gui.locateOnScreen("./image/ling_xian_24.png", confidence=0.8))
+        box = tuple[int, int, int, int](gui.locateOnScreen("./image/ling_xian_24.png", confidence=0.9))
         gui.click(gui.center(box))
-        return process_content(get_content())
+        return process_body(get_content())
     except gui.ImageNotFoundException as e:
         return e
 
@@ -44,26 +50,44 @@ def get_content():
     gui.hotkey("ctrl", "c")
     import win32clipboard as w
     w.OpenClipboard()
-    content = w.GetClipboardData()
+    try:
+        content = w.GetClipboardData()
+    except Exception:
+        content = ""
     w.CloseClipboard()
     return content
 
 
-def process_content(content):
+def process_body(content):
     print(content)
-    str_list = cont.content_filter(content)
-    if len(str_list) == 0:
-        return False
-    if len(str_list) >= 1:
-        if not server.Analysis_Header(str_list[0], ""):
-            return False
-    if len(str_list) == 2:
-        return server.Analysis_Body(str_list[1], "")
-    if len(str_list) >= 3:
-        return server.Analysis_Body(str_list[1], str_list[2])
-    return server.Analysis_Body("", "")
+    str_list = tools.content_filter(content)
+    body = server.Body
+    for i in range(len(str_list)):
+        if i == 0:
+            if not server.Header.Analysis(str_list[0]):
+                return False
+        else:
+            unknown = body.Analysis(str_list[i])
+            if tools.type_name(unknown, "Query"):
+                body = unknown
+            if tools.type_name(unknown, "str"):
+                return unknown
+            if tools.type_name(unknown, "int"):
+                if unknown == cst.ID_DRINK:
+                    return process_drink(str_list[i:])
+    return body.Analysis("")
+
+
+def process_drink(str_list):
+    print(str_list)
+    drink = server_drink.Drink
+    for i in range(len(str_list)):
+        drink = drink.Search_Drink(str_list[i])
+    print(drink.__str__())
+    return drink.__str__()
 
 
 if __name__ == '__main__':
     server.init()
+    server_drink.drink_init()
     server_start()
